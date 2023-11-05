de2fbd4e8fbafd9b9cd24836d06f9948cd41b543
abhishek dhanwal <kft.abhishek@gmail.com>
2023-11-05 16:45:31 +0530

    driver model

--- code/app/Models/Driver.php
+++ code/app/Models/Driver.php
@@ -17,7 +17,7 @@ class Driver extends Authenticatable
     protected $table = 'driver_details';
 	protected $primaryKey = 'driverid';
 
-    protected $fillable = ['driverid', 'username', 'password', 'signupdate','email','phone','businessurl','4seatervehicle','8seatervehicle','estatevehicle','courier','easyaccessvehicle','airportruns','offtime_timestamp','offtime_timestamp_created_at','dutystatus'];
+    protected $fillable = ['driverid', 'username', 'password', 'signupdate','email','phone','businessurl','4seatervehicle','8seatervehicle','estatevehicle','courier','easyaccessvehicle','airportruns','offtime_timestamp','offtime_timestamp_created_at','dutystatus','firstname','lastname'];
 	
 	protected $guard = 'driveruser';
 
